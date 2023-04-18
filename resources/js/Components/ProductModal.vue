63b3fa75379a32a5426383648378c4f38185f90b
Exoezeroes <exoezeroes@gmail.com>
2023-04-18 10:36:32 +0800

    Buttons layout for edit and delete
    the route has yet to exist

--- resources/js/Components/ProductModal.vue
+++ resources/js/Components/ProductModal.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { useProductStore } from "@/Stores/product";
+import { mdiDelete, mdiPencil } from "@mdi/js";
+import BaseButton from "./BaseButton.vue";
+import BaseButtons from "./BaseButtons.vue";
 import CardBox from "@/Components/CardBox.vue";
 import CardBoxModal from "@/Components/CardBoxModal.vue";
 import ProductModalTable from "./ProductModalTable.vue";
@@ -9,12 +12,21 @@ const productStore = useProductStore();
 
 <template>
   <CardBoxModal
-    button="success"
-    button-label="Save"
-    hasCancel
     v-model="productStore.modalActive"
     :title="productStore.productActive.title"
   >
+    <BaseButtons type="justify-between">
+      <BaseButton
+        :icon="mdiPencil"
+        color="warning"
+        outline
+      />
+      <BaseButton
+        :icon="mdiDelete"
+        color="danger"
+        outline
+      />
+    </BaseButtons>
     <CardBox has-table>
       <ProductModalTable :product="productStore.productActive" />
     </CardBox>
