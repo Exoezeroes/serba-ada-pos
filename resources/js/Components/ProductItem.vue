ebbad56ee921fe904542e1a66ba0fc9eaf43df0f
Exoezeroes <exoezeroes@gmail.com>
2023-05-17 20:56:01 +0800

    Added simple logic in ProductItem
    this is to avoid blank

--- resources/js/Components/ProductItem.vue
+++ resources/js/Components/ProductItem.vue
@@ -9,10 +9,10 @@ const props = defineProps({
 
 <template>
   <div class="flex flex-col place-items-center">
-    <div class="line-clamp-1">uid: {{ product.uid }}</div>
-    <div class="line-clamp-1">title: {{ product.title }}</div>
-    <div class="line-clamp-1">qty: {{ product.quantity }}</div>
-    <div class="line clamp-1">buy: {{ product.buy_price }}</div>
-    <div class="line clamp-1">sell: {{ product.sell_price }}</div>
+    <div v-if="product.uid" class="line-clamp-1">uid: {{ product.uid }}</div>
+    <div v-if="product.title" class="line-clamp-1">title: {{ product.title }}</div>
+    <div v-if="product.quantity" class="line-clamp-1">qty: {{ product.quantity }}</div>
+    <div v-if="product.buy_price" class="line clamp-1">buy: {{ product.buy_price }}</div>
+    <div v-if="product.sell_price" class="line clamp-1">sell: {{ product.sell_price }}</div>
   </div>
 </template>
